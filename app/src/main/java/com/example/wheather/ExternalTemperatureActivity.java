2b30174e40c607ca86c4acde145a58f1c850a3c8
anderson-robert <robertallandavidanderson@gmail.com>
2022-01-17 10:32:25 +0100

    feat: Added right swiping gestures to ExternalTemperatureActivity

--- app/src/main/java/com/example/wheather/ExternalTemperatureActivity.java
+++ app/src/main/java/com/example/wheather/ExternalTemperatureActivity.java
@@ -1,7 +1,9 @@
 package com.example.wheather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -40,6 +42,7 @@ public class ExternalTemperatureActivity extends AppCompatActivity {
     private static final String OpenWeatherAPIURL = "api.openweathermap.org/data/2.5/weather?q=Zurich&units=metric&appid=337567a0234864c15055bd00c1fd627e";
     private static final String OpenWeatherAPITempTerm = "temp";
     private TextView temperatureField;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,18 @@ public class ExternalTemperatureActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        constraintLayout = (ConstraintLayout) findViewById(R.id.et_constraint);
+        constraintLayout.setOnTouchListener(new OnSwipeTouchListener(this){
+            public void onSwipeRight() {
+                switchActivity();
+            }
+        });
+    }
+
+    //Method to start a new Activity
+    public void switchActivity() {
+        Intent intent = new Intent(this, RoomTemperatureActivity.class);
+        startActivity(intent);
     }
 
     private void getTemperatureFromAPI() throws IOException {
