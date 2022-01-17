db2a3d985d09c451bbed208851f5110d476baf88
anderson-robert <robertallandavidanderson@gmail.com>
2022-01-17 08:57:09 +0100

    test: Not currently working: get external room temp from OpenWeatherAPI

--- app/src/main/java/com/example/wheather/ExternalTemperatureActivity.java
+++ app/src/main/java/com/example/wheather/ExternalTemperatureActivity.java
@@ -3,8 +3,26 @@ package com.example.wheather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.JsonReader;
 import android.widget.TextView;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -13,10 +31,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ExternalTemperatureActivity extends AppCompatActivity {
 
-    private static final String OpenWeatherAPIURL = "api.openweathermap.org/data/2.5/weather?q=Zurich&appid=337567a0234864c15055bd00c1fd627e";
+    private static final String OpenWeatherAPIURL = "api.openweathermap.org/data/2.5/weather?q=Zurich&units=metric&appid=337567a0234864c15055bd00c1fd627e";
     private static final String OpenWeatherAPITempTerm = "temp";
     private TextView temperatureField;
 
@@ -25,35 +46,36 @@ public class ExternalTemperatureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_external_temperature);
         temperatureField = (TextView) findViewById(R.id.et_temp);
-        getTemperatureFromAPI();
-    }
-
-    private void getTemperatureFromAPI() {
-        String returnedTemp;
         try {
-            URL url = new URL(OpenWeatherAPIURL);
-            HttpURLConnection connection= (HttpURLConnection)url.openConnection();
-            InputStream inputStream = new BufferedInputStream(connection.getInputStream());
-            JsonReader reader =new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
-            reader.beginObject();
-            reader.nextName();
-            reader.beginArray();
-            reader.beginObject();
-            while (reader.hasNext()) {
-                String name = reader.nextName();
-                if (name.equals(OpenWeatherAPITempTerm)) {
-                    returnedTemp = String.valueOf(reader.nextDouble());
-                    temperatureField.setText(returnedTemp);
-                } else {
-                    reader.skipValue();
-                }
-            }
-            reader.close();
-            inputStream.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+            getTemperatureFromAPI();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void getTemperatureFromAPI() throws IOException {
+        String returnedTemp = "";
+        URL url = new URL(OpenWeatherAPIURL);
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+        OpenWeatherJson opj = new Gson().fromJson(reader, OpenWeatherJson.class);
+
+        returnedTemp = opj.main.get("temp");
+        returnedTemp = "7";
+        temperatureField.setText("7");
+    }
+
+    private class OpenWeatherJson {
+        Map<String, Float> coord;
+        Map<String, String> main;
+        Map<String, String> wind;
+        String base;
+        String visibility;
+        Map<String, Float> clouds;
+        Integer dt;
+        Map<String, String> sys;
+        String timezone;
+        Integer id;
+        String name;
+        Integer cod;
     }
 }
